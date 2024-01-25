1ad7d8ccaf52d1ba462db47bc9abf9ae41816d02
Jess :) <40944940+jesslogan@users.noreply.github.com>
2024-01-25 16:05:20 +0000

    isolating issue to blob

--- app.py
+++ app.py
@@ -6,20 +6,20 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 
-from azure.identity import DefaultAzureCredential
-from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+# from azure.identity import DefaultAzureCredential
+# from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
-credential = DefaultAzureCredential()
-account_url = "https://cofcstorage.blob.core.windows.net"
-blob_service_client = BlobServiceClient(account_url, credential = credential)
+# credential = DefaultAzureCredential()
+# account_url = "https://cofcstorage.blob.core.windows.net"
+# blob_service_client = BlobServiceClient(account_url, credential = credential)
 
-container_name = "test"
+# container_name = "test"
 
-try:
-    container_client = blob_service_client.get_container_client(container = container_name)
-    container_client.get_container_properties()
-except Exception as e:
-    container_client = blob_service_client.create_container(container_name)
+# try:
+#     container_client = blob_service_client.get_container_client(container = container_name)
+#     container_client.get_container_properties()
+# except Exception as e:
+#     container_client = blob_service_client.create_container(container_name)
 
 
 app = Flask(__name__, static_folder='static')
