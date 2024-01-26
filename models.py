024abf21b8e421a25bc42bd2b1b3201e1606e239
Jess :) <40944940+jesslogan@users.noreply.github.com>
2024-01-26 17:03:23 +0000

    oopsie pt2

--- models.py
+++ models.py
@@ -1,14 +1,14 @@
 from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
 from sqlalchemy.orm import validates
 
-from app import db
+
 
 class user(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(200), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
-    name = db.Column(db.String(200))
-    company = db.Column(db.String(200), unique=True, nullable=False)
+    id = Column(Integer, primary_key=True)
+    email = Column(String(200), unique=True, nullable=False)
+    password = Column(String(200), nullable=False)
+    name = Column(String(200))
+    company = Column(String(200), unique=True, nullable=False)
     
 
 class Restaurant(db.Model):
