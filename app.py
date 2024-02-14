24ae91f067fc6e5fc81b1f3b24e1d3fa0c0d2e14
Mark Dean <37253528+upskydownshoes@users.noreply.github.com>
2024-02-14 13:13:54 +0000

    pls

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def index():
 @app.route('/send', methods=['GET', 'POST'])
 def send():
     form = SendCertificates()
-    new_entry = send_certificates(
+    new_entry = SendCertificates(
             recipient=form.recipient.data,
             po_number=form.po_number.data,
             batch_number=form.batch_number.data,
